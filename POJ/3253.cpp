1725ddb57b15a3dc15329f9f447390088f253950
kaixiang4515 <kaixiang4515@gmail.com>
2019-01-05 15:21:09 +0800

    update

--- POJ/3253.cpp
+++ POJ/3253.cpp
@@ -1,27 +1,37 @@
 #include <cstdio>
 //#include <cstring>
-#include <algorithm>
+//#include <algorithm>
 //#include <ctime>
+//#include <bits/stdc++.h>
+#include <queue>
 using namespace std;
-long long a[20000];
+typedef long long LL;
+int n;
+void solve(){
+    priority_queue<LL, vector<LL>, greater<LL> > mnpq;
+    int in;
+    LL s=0;
+    for(int i=0;i!=n;++i){
+        scanf("%d",&in);
+        mnpq.push(in);
+    }
+    LL a,b;
+    while(n>1){
+        a=mnpq.top(); mnpq.pop();
+        b=mnpq.top(); mnpq.pop();
+        s+=a+b;
+        mnpq.push(a+b);
+        --n;
+    }
+    printf("%lld\n",s);
+}
 int main()
 {
     //freopen("..\\file\\input.txt","r",stdin);
     //freopen("..\\file\\output.txt","w",stdout);
     /*clock_t start_c,end_c;
     start_c=clock();*/
-    int n;
-    long long ans=0;
-    scanf("%d",&n);
-    for(int i=0;i!=n;++i) scanf("%lld",a+i);
-    sort(a,a+n);
-    for(int k=n;k!=1;--k){
-        int i=n-k+1;
-        a[i]+=a[i-1];
-        ans+=a[i];
-        for(;i!=n-1 && a[i]>a[i+1];++i) swap(a[i],a[i+1]);
-    }
-    printf("%lld",ans);
+    while(~scanf("%d",&n)) solve();
     /*end_c=clock();
     printf("Use %.0lf ms\n", (((double)(end_c-start_c))/CLOCKS_PER_SEC)*1000);*/
     return 0;
